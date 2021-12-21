b559dab17a552e88f521c0c5428c423149d6cdc8
RinZ26 <ivs1996@mail.ru>
2021-12-21 17:25:06 +0500

    0. Spring Liquibase [#302330]

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -3,7 +3,7 @@ create table posts
     id          serial primary key,
     name        varchar(2000),
     description text,
-    created     timestamp not null
+    created     timestamp not null default now()
 );
 
 create table users
